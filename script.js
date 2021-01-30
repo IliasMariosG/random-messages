ec0490f35908f5375747a412672e6beeb161b905
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-30 13:16:47 +0200

    Add messages object

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function generateRandomNumber(number) {
+function generateRundomNumber(number) {
   return Math.floor(Math.random() * number)
+}
+
+const messages = {
+  sign: ['sun', 'moon'],
+  fortune: ['good luck', 'ok luck'],
+  advice: ['trust some', 'do what you need to']
 }
